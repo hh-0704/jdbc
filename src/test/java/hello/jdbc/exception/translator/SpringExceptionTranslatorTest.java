971f42d69f6c19a3b0b34e6273f83b8513776c8e
hh-0704 <ghksl0704@naver.com>
2026-07-23 16:24:02 +0900

    7 - 53 스프링 예외 추상화 이해

---
+++ src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -0,0 +1,54 @@
+package hello.jdbc.exception.translator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class SpringExceptionTranslatorTest {
+
+    DataSource dataSource;
+
+    @BeforeEach
+    void init() {
+        dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    }
+
+    @Test
+    void sqlExceptionErrorCode() {
+        String sql = "select bad grammar";
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.executeQuery();
+        } catch (SQLException e) {
+            Assertions.assertThat(e.getErrorCode()).isEqualTo(42122);
+            //org.springframework.jdbc.support.sql-codes.xml
+            SQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+
+            //org.springframwork.jdbc.BadSqlGrammarException
+            DataAccessException resultEx = exTranslator.translate("select", sql, e);
+            log.info("resultEx", resultEx);
+            Assertions.assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
+
+//            int errorCode = e.getErrorCode();
+//            log.info("errorCode={}", e.getErrorCode());
+            //org.h2.jdbc.JdbcSQLSyntaxErrorException
+//            log.info("error", e);
+        }
+    }
+}
